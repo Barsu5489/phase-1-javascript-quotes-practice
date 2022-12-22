133fb3e081344810b72b801d37d4ef8923b072e0
barsu5489 <ebarsulai@gmail.com>
2022-12-23 01:04:16 +0300

    quotes js practice completed

--- src/index.js
+++ src/index.js
@@ -1,119 +1,112 @@
-function fetchData()
-{
-    fetch('http://localhost:3000/quotes?_embed=likes')
-    .then(res=>
+const quotes = 'http://localhost:3000/quotes?_embed=likes'
+
+function fetchQuotes(){
+    fetch(quotes)
+    .then(res=> res.json())
+    .then(quotes=>{
+        console.log(quotes)
+        quoteStructure(quotes)
+    })
+}
+
+function quoteStructure(data){
+    data.forEach(data=>{
+
+    const quoteList = document.getElementById('quote-list');
+
+    const quoteCard = document.createElement('li');
+    quoteCard.classList.add('quoteCard');
+
+    const blockQuote = document.createElement('blockquote');
+    blockQuote.classList.add('blockquote');
+
+    const p = document.createElement('p');
+    p.classList.add('mb-0');
+    p.textContent = data.quote;
+
+    const footer = document.createElement('footer');
+    footer.classList.add('blockquote-footer');
+    footer.textContent = data.author
+
+    const br = document.createElement('br');
+
+    const likeBtn = document.createElement('button');
+    likeBtn.classList.add('btn-success');
+    likeBtn.textContent = `Likes: ` 
+
+    const span = document.createElement('span');
+    span.textContent = 0;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('btn-danger');
+    deleteBtn.textContent = 'Delete';
+
+    blockQuote.appendChild(p)
+    blockQuote.appendChild(footer)
+    blockQuote.appendChild(br)
+    likeBtn.appendChild(span)
+    blockQuote.appendChild(likeBtn)
+    blockQuote.appendChild(deleteBtn)
+    quoteCard.appendChild(blockQuote)
+    quoteList.appendChild(quoteCard)   
+    console.log(data.id)     
+    const id = data.id
+    deleteBtn.addEventListener('click',(e)=>{
+        e.preventDefault()
+        fetch(`http://localhost:3000/quotes/${data.id}`,
         {
-            return res.json();
+            method:'delete'
         })
-    .then(data=>
+    })
+
+  likeBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    span.textContent ++
+    like(id)
+  })
+})
+
+}
+function tst(){
+
+    const addNewQuote = document.getElementById('new-quote-form');
+    addNewQuote.addEventListener('submit',(e)=>{
+        e.preventDefault()
+      const postQ = new FormData(addNewQuote)
+      const postQuote =  new URLSearchParams(postQ)
+      console.log([...postQuote])
+      
+        fetch('http://localhost:3000/quotes',
         {
-            quotes(data)
+            method:'post',
+            body:postQuote
+        })
+        .then(res=>res.json())
+        .then(data=>{
             console.log(data)
         })
-    
-}
 
-// function liker()
-// {
-//     let likes = document.querySelector('.btn-success');
-//     likes.addEventListener('click', ()=>
-//     {
-//         let plus = likes.textContent+1
-    
-//     fetch('http://localhost:3000/likes',
-//     {
-//         method: 'POST',
-//         headers:
-//         {
-//             'Accept': 'Application/json'
-//         },
-//         body: JSON.stringify({quoteId:plus})
-//     })
-//     .then(res => res.json())
-//     .then(data => console.log(data))
-// })
-//}
-function postQuote()
-{
-    const form = document.getElementById('new-quote-form')
-    form.addEventListener('submit', (e)=>
-    {
-        e.preventDefault()
-        const quotes = e.target.quote.value 
-        const name = e.target.author.value
-        const addData = document.getElementById('quote-list')
-        addData.innerHTML += `<li class='quote-card'>
-                    <blockquote class="blockquote">
-                      <p class="mb-0">${quotes}</p>
-                      <footer class="blockquote-footer">${name}</footer>
-                      <br>
-                      <button class='btn-success'>Likes: <span>0</span></button>
-                      <button class='btn-danger'>Delete</button>
-                    </blockquote>
-                  </li>`
     })
-    
 }
-function quotes(data)
-{
-   
-        let data1 ="";
-           data.map((dat)=>
-           {
-                data1 += `<li class='quote-card'>
-                    <blockquote class="blockquote">
-                      <p class="mb-0">${dat.quote}</p>
-                      <footer class="blockquote-footer">${dat.id} ${dat.author}</footer>
-                      <br>
-                      <button class='btn-success'>Likes:${dat.likes} <span>0</span></button>
-                      <button class='btn-danger'>Delete</button>
-                    </blockquote>
-                  </li>`
-           })
-           const quoteList = document.getElementById('quote-list')
-           quoteList.innerHTML = data1
 
-    
+async function like(){
+
+
+    const likesUpStream = {
+        method:'post',
+        headers: { 'Content-Type':'application/json' },
+        body: JSON.stringify({
+            quoteId: 5,
+            createdAt: Date.now()
+        })
+    }
+    const res = await fetch('http://localhost:3000/likes', likesUpStream)
+    const data = await res.json()
+    console.log(data)
 }
-document.addEventListener('DOMContentLoaded',()=>
-{
-    fetchData()
-    postQuote()
-    
-})
-// 
-//    const quoteList = document.getElementById('quote-list')
-//    quoteList.innerHTML = data1
-    // data.forEach((dat)=>
-    // {
-    //     const cards = document.getElementById('cards')
-    //     const quoteList = document.getElementById('quote-list')
-    //     quoteList.innerHTML += 
-    //     `
-    //     <li class='quote-card'>
-    //     <blockquote class="blockquote">
-    //       <p class="mb-0">${dat.quote}</p>
-    //       <footer class="blockquote-footer">${dat.author}</footer>
-    //       <br>
-    //       <button class='btn-success'>Likes:${dat.likes} <span>0</span></button>
-    //       <button class='btn-danger'>Delete</button>
-    //     </blockquote>
-    //   </li>
-    //     `
-    //     cards.appendChild(quoteList)
-    //     return(dat)
-    // })
 
-//     quoteList.innerHTML = 
-//     `
-//     <li class='quote-card'>
-//     <blockquote class="blockquote">
-//       <p class="mb-0">${data[i].quote}</p>
-//       <footer class="blockquote-footer">${data[i].author}</footer>
-//       <br>
-//       <button class='btn-success'>Likes:${data[i].likes} <span>0</span></button>
-//       <button class='btn-danger'>Delete</button>
-//     </blockquote>
-//   </li>
-//     `
-//     cards.appendChild(quoteList)
+
+
+tst()
+
+fetchQuotes()
